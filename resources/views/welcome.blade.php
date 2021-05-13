2bb1c4f81cb4026a40b5a7eb63d0ad42b3870353
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-05-13 18:58:23 +0200

    table print

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,20 +80,13 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+              <ul>
+              @foreach ($movies as $movie)
+              <li>{{$movie->title}}</li>
+              <li>{{$movie->genre}}</li>
+              <li>{{$movie->author}}</li><br>
+              @endforeach
+            </ul>
             </div>
         </div>
     </body>
